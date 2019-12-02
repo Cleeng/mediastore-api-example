0087220e205d4baded100c78c3ba2dbf00a37ae3
Katarzyna Zemka <kzemka@cleeng.com>
2019-12-02 17:49:30 +0100

    1.0.0

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true
+  },
+  extends: ['airbnb', 'plugin:prettier/recommended', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    document: true,
+    ENVIRONMENT_CONFIGURATION: true
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'jest'],
+  rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }]
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src']
+      }
+    }
+  }
+};
---
+++ config/environments/production.js
@@ -0,0 +1,11 @@
+module.exports = {
+  ENVIRONMENT_CONFIGURATION: {
+    GB_API_URL: JSON.stringify('https://connect-sandbox.cleeng.com'),
+    ADYEN_PUBLIC_KEY: {
+      'https://webstoresdk-demo.cleeng.com': JSON.stringify(
+        'pub.v2.7814375173870120.aHR0cDovL3dlYnN0b3Jlc2RrLWRlbW8uY2xlZW5nLmNvbQ.jhFD9Q1Pq5vjHmkL1o13YNGRqtOGHiT-fUcp26yHCJg'
+      )
+    },
+    WEB_API: JSON.stringify('https://sandbox.cleeng.com')
+  }
+};
